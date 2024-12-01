d71ca257249e0adc2fe5975dec2b4bdf8ee551ac
Nebras Khan <khann84@mcmaster.ca>
2024-11-30 20:26:59 -0500

    Add initial project files

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get the login button
+const loginButton = document.getElementById('loginButton');
+
+// Add event listener for login button click
+loginButton.addEventListener('click', () => {
+  // Redirect directly to the home screen
+  window.location.href = "home.html"; // Replace with the actual Home Screen file path
+});
